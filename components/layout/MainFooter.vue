688b19fee285f1d687b4ecd428a529e82aa534ad
Alejandro Ramirez Garcia <76931084+ale9420@users.noreply.github.com>
2024-07-30 22:03:14 -0500

    se agrega componente footer, mejoras responsive para el header

--- components/layout/MainFooter.vue
+++ components/layout/MainFooter.vue
@@ -1,6 +1,26 @@
 <template>
-  <footer class="bg-gradient-to-r from-slate-500 to-gray-600 p-5">
-    footer
+  <footer
+    class="flex sm:flex-col items-center bg-gradient-to-r from-slate-500 to-gray-600 p-5 text-neutral-50"
+  >
+    <div class="flex gap-3">
+      <NuxtLink to="https://github.com/ale9420">
+        <NuxtImg class="h-10 w-10" src="/github.png" />
+      </NuxtLink>
+      <NuxtLink
+        to="https://www.linkedin.com/in/alejandro-ramirez-garcia-046713139/"
+      >
+        <NuxtImg class="h-10 w-10" src="/linkedin.png" />
+      </NuxtLink>
+    </div>
+    <hr class="w-full my-5" />
+    <div class="container text-justify">
+      <h3 class="text-xl">Blog</h3>
+      <p class="text-sm mt-2">
+        El objetivo de este blog es de hablar de mis experiencias en el mundo
+        del desarrollo web, noticias, tutoriales y poder compartir el
+        conocimiento con quien lo desee.
+      </p>
+    </div>
   </footer>
 </template>
 
--- components/layout/MainHeader.vue
+++ components/layout/MainHeader.vue
@@ -1,9 +1,10 @@
 <template>
   <header
-    class="sticky top-0 flex items-center justify-between sm:p-2 lg:p-5 bg-gradient-to-r from-red-600 to-red-400 w-full z-10"
+    class="sticky top-0 flex items-center justify-between sm:p-3 lg:p-5 bg-gradient-to-r from-red-600 to-red-400 w-full z-10"
   >
+    <Bars3Icon class="lg:hidden size-6 text-neutral-50" />
     <h3 class="text-slate-50 text-xl">BOG.DEV</h3>
-    <div class="flex">
+    <div class="sm:hidden lg:flex">
       <NuxtLink
         to="/"
         class="text-slate-50 hover:underline hover:underline-offset-4 hover:cursor-pointer p-2"
@@ -22,6 +23,7 @@
 
 <script lang="ts" setup>
 import type { PageEntityResponseCollection } from '~/types'
+import { Bars3Icon } from '@heroicons/vue/16/solid'
 import pagesQuery from '@/graphql/queries/pages.gql'
 
 const graphql = useStrapiGraphQL()
